3824df9c8c74e221b47fcb894b1b28bc006d2dae
Lívia Oliveira da Silva <livia0506207os@gmail.com>
2026-04-27 12:15:57 -0300

    Adicionando todas as rotas

--- app.py
+++ app.py
@@ -6,16 +6,33 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
-
 @app.route("/agendamento")
 def agendamento():
     return render_template("agendamento.html")
 
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/saibamais")
+def saibamais():
+    return render_template("saibamais.html")
+
+@app.route("/mapa")
+def mapa():
+    return render_template("mapa.html")
+
+@app.route("/admin")
+def admin():
+    return render_template("admin_locais.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
 
